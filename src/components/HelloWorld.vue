b81e19a08c7a02744dc56a46f1eb1e97ed290e53
SPAdmin <SPAdmin@NPTEST.COM>
2019-07-18 17:37:11 +0900

    change UI component importing

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,49 +1,46 @@
 <template>
   <div class="full-control">
     <span class="md-display-4">시계</span>
-<div class="md-layout-item">
-        <md-field>
-          <md-select v-model="country" name="country" id="country" placeholder="중요도">
-            <md-option value="1">높음</md-option>
-            <md-option value="2">보통</md-option>
-            <md-option value="3">낮음</md-option>
-          </md-select>
-        </md-field>
-      </div>
-      <div>
+    <div class="md-layout-item">
       <md-field>
-      <label>Type here!</label>
-      <md-input v-model="type"></md-input>
-    </md-field>
-      </div>
-      <div><md-button class="md-icon-button">
+        <md-select v-model="priority" name="priority" id="priority" placeholder="중요도">
+          <md-option value="1">높음</md-option>
+          <md-option value="2">보통</md-option>
+          <md-option value="3">낮음</md-option>
+        </md-select>
+      </md-field>
+    </div>
+    <div>
+      <md-field>
+        <label>Type here!</label>
+        <md-input v-model="type"></md-input>
+      </md-field>
+    </div>
+    <div>
+      <md-button class="md-icon-button">
         <md-icon>add</md-icon>
       </md-button>
-      </div>
-      <div>
-    <md-list>
-      <md-subheader>Notifications</md-subheader>
-
-      <md-list-item>
-        <md-checkbox v-model="notification" value="preview" />
-        <span class="md-list-item-text">Show content preview</span>
-      </md-list-item>
-
-      <md-list-item>
-        <md-checkbox v-model="notification" value="sound" />
-        <span class="md-list-item-text">Sound</span>
-      </md-list-item>
-
-      <md-list-item>
-        <md-checkbox v-model="notification" value="vibrate" />
-        <span class="md-list-item-text">Vibrate</span>
-      </md-list-item>
-
-      <md-list-item>
-        <md-checkbox v-model="notification" value="light" />
-        <span class="md-list-item-text">Notification light</span>
-      </md-list-item>
-    </md-list>
+    </div>
+    <div>
+      <md-list>
+        <md-subheader>Notifications</md-subheader>
+        <md-list-item>
+          <md-checkbox v-model="notification" value="preview" />
+          <span class="md-list-item-text">Show content preview</span>
+        </md-list-item>
+        <md-list-item>
+          <md-checkbox v-model="notification" value="sound" />
+          <span class="md-list-item-text">Sound</span>
+        </md-list-item>
+        <md-list-item>
+          <md-checkbox v-model="notification" value="vibrate" />
+          <span class="md-list-item-text">Vibrate</span>
+        </md-list-item>
+        <md-list-item>
+          <md-checkbox v-model="notification" value="light" />
+          <span class="md-list-item-text">Notification light</span>
+        </md-list-item>
+      </md-list>
     </div>
   </div>
 </template>
@@ -52,7 +49,9 @@
 export default {
   name: 'Controls',
   data: () => ({
-    notification: ['sound', 'vibrate']
+    notification: ['sound', 'vibrate'],
+    priority: null,
+    type: null
   })
 }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import { MdButton, MdContent, MdTabs } from 'vue-material/dist/components'
+import { MdButton, MdContent, MdTabs, MdField, MdCheckbox, MdList, MdMenu, MdSubheader } from 'vue-material/dist/components/'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
-import VueMaterial from 'vue-material'
 
 Vue.use(Router)
 Vue.use(MdButton)
 Vue.use(MdContent)
 Vue.use(MdTabs)
-Vue.use(VueMaterial)
+Vue.use(MdField)
+Vue.use(MdCheckbox)
+Vue.use(MdList)
+Vue.use(MdMenu)
+Vue.use(MdSubheader)
 
 export default new Router({
   routes: [
