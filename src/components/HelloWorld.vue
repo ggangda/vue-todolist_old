aebfa8e1d1219c9a42bea7649d0c62cd4f3c1a81
SPAdmin <SPAdmin@NPTEST.COM>
2019-07-18 17:48:58 +0900

    UI 수정중....

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,25 +1,27 @@
 <template>
   <div class="full-control">
     <span class="md-display-4">시계</span>
-    <div class="md-layout-item">
-      <md-field>
-        <md-select v-model="priority" name="priority" id="priority" placeholder="중요도">
-          <md-option value="1">높음</md-option>
-          <md-option value="2">보통</md-option>
-          <md-option value="3">낮음</md-option>
-        </md-select>
-      </md-field>
-    </div>
-    <div>
-      <md-field>
-        <label>Type here!</label>
-        <md-input v-model="type"></md-input>
-      </md-field>
-    </div>
-    <div>
-      <md-button class="md-icon-button">
-        <md-icon>add</md-icon>
-      </md-button>
+    <div class="md-layout md-gutter">
+      <div class="md-layout-item">
+        <md-field>
+          <md-select v-model="priority" name="priority" id="priority" placeholder="중요도">
+            <md-option value="1">높음</md-option>
+            <md-option value="2">보통</md-option>
+            <md-option value="3">낮음</md-option>
+          </md-select>
+        </md-field>
+      </div>
+      <div class="md-layout-item">
+        <md-field>
+          <label>Type here!</label>
+          <md-input v-model="type"></md-input>
+        </md-field>
+      </div>
+      <div class="md-layout-item">
+        <md-button class="md-icon-button">
+          <md-icon>add</md-icon>
+        </md-button>
+      </div>
     </div>
     <div>
       <md-list>
