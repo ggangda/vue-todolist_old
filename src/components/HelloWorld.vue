e1fd4b39ab94d6db2b46d0dc33184bdee319e12d
SPAdmin <SPAdmin@NPTEST.COM>
2019-07-22 14:16:37 +0900

    UI 정렬 완료

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="full-control">
-    <span class="md-display-4">시계</span>
+    <span id="clock" class="md-display-4">시계 {{ time }}</span>
     <div class="md-layout md-gutter">
-      <div class="md-layout-item">
+      <div class="md-layout-item md-size-15">
         <md-field>
           <md-select v-model="priority" name="priority" id="priority" placeholder="중요도">
             <md-option value="1">높음</md-option>
@@ -11,13 +11,13 @@
           </md-select>
         </md-field>
       </div>
-      <div class="md-layout-item">
+      <div class="md-layout-item md-size-75">
         <md-field>
           <label>Type here!</label>
           <md-input v-model="type"></md-input>
         </md-field>
       </div>
-      <div class="md-layout-item">
+      <div class="md-layout-item md-size-10">
         <md-button class="md-icon-button">
           <md-icon>add</md-icon>
         </md-button>
@@ -53,15 +53,20 @@ export default {
   data: () => ({
     notification: ['sound', 'vibrate'],
     priority: null,
-    type: null
+    type: null,
+    time : ''
   })
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
+  .full-control{
+    margin: 0 20% 0 20%;
+    position: relative;
+  }
   .md-list {
-    width: 320px;
+    width: 100%;
     max-width: 100%;
     display: inline-block;
     vertical-align: top;
